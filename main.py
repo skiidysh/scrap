1c4dd35d591859a6e294d59921ec39b1f436ebc9
skiidysh <75967605+skiidysh@users.noreply.github.com>
2024-04-04 23:04:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ def save_to_db(data):
     conn.close()
 
 if __name__ == '__main__':
-    url = 'https://www.citilink.ru/catalog/igrovye-noutbuki/?pf=discount.any%2Crating.any&f=discount.any%2Crating.any%2C277_3ryzend15%2C277_3ryzend17%2C277_3ryzend19'
     html = get_html(url)
     data = parse_page(html)
     save_to_db(data)
