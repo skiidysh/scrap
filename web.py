925b643946ad6700da822ee7f28638491a11c5d1
skiidysh <75967605+skiidysh@users.noreply.github.com>
2024-04-04 23:25:59 +0500

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    conn = sqlite3.connect('laptops.db')
+    
     cursor = conn.cursor()
     
     # Получаем данные из базы данных, сортируя ноутбуки по увеличению цены
