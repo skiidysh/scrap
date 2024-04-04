1946d726bf81b0f627ee03374121b59a14bcd9c7
skiidysh <75967605+skiidysh@users.noreply.github.com>
2024-04-04 23:31:19 +0500

    Update web.py

--- web.py
+++ web.py
@@ -17,4 +17,4 @@ def index():
     return render_template('index.html', laptops=laptops)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port = 5000, host = "0.0.0.0", debug = True)
